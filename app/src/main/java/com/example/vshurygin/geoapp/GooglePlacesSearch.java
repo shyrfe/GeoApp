cc44ab44d7ceb00831dfb3b0df8cb2a48a71ba65
shyrfe <shyrfe@yandex.ru>
2016-08-15 16:00:16 +0300

    fix some bug with map

--- app/src/main/java/com/example/vshurygin/geoapp/GooglePlacesSearch.java
+++ app/src/main/java/com/example/vshurygin/geoapp/GooglePlacesSearch.java
@@ -21,13 +21,15 @@ public class GooglePlacesSearch
 {
     private PlaceInfo[] mPlaces = null;
     private final String GOOGLE_API_PLACE_SEARCH_URL = "https://maps.googleapis.com/maps/api/place/search/json?";
+    private final String GOOGLE_API_PLACE_DETAILS_SEARCH_URL = "https://maps.googleapis.com/maps/api/place/details/json?";
+
 
     GooglePlacesSearch() {}
 
 
     public void setPlaces(double _latitude, double _longitude, double _radius, String _types)
     {
-        new ProgressTask(GOOGLE_API_PLACE_SEARCH_URL,_latitude,_longitude,_radius,_types).execute();
+        new FindPlaces(GOOGLE_API_PLACE_SEARCH_URL,GOOGLE_API_PLACE_DETAILS_SEARCH_URL,_latitude,_longitude,_radius,_types).execute();
     }
 
     public PlaceInfo[] getPlaces()
@@ -40,22 +42,27 @@ public class GooglePlacesSearch
     }
 
 
-    class ProgressTask extends AsyncTask<String,Void,String>
+    class FindPlaces extends AsyncTask<String,Void,String>
     {
+        private final String KEY = "AIzaSyCHpwqjnrTJVCh4gv0szpdibd6KGl5_vjg";
+
         private String mURL;
+        private String mDetailsSearchURL;
         private double mLatitude;
         private double mLongitude;
         private double mRadius;
         private String mTypes;
 
-        ProgressTask(String _url, double _latitude, double _longitude, double _radius, String _types )
+        FindPlaces(String _url,String _detailSearchURL, double _latitude, double _longitude, double _radius, String _types )
         {
             mURL = _url;
+            mDetailsSearchURL = _detailSearchURL;
             mLatitude = _latitude;
             mLongitude = _longitude;
             mRadius = _radius;
             mTypes = _types;
         }
+
         private String prepareURL()
         {
             StringBuilder result = new StringBuilder();
@@ -69,7 +76,7 @@ public class GooglePlacesSearch
             {
                 result.append("type="+mTypes+"&");
             }
-            result.append("key=AIzaSyCHpwqjnrTJVCh4gv0szpdibd6KGl5_vjg");
+            result.append("key="+KEY);
 
             return result.toString();
         }
@@ -163,6 +170,7 @@ public class GooglePlacesSearch
                         ));
                     }
 
+                    new LoadDetailInfo(mDetailsSearchURL,placesArray.get(i).getPlaceId(),placesArray.get(i)).execute();
                     //Log.d("PlacesSearch",results.getJSONObject(i).getString("name"));
                 }
 
@@ -177,4 +185,100 @@ public class GooglePlacesSearch
         }
     }
 
+    class LoadDetailInfo extends AsyncTask<String,Void,String>
+    {
+        private final String KEY = "AIzaSyCHpwqjnrTJVCh4gv0szpdibd6KGl5_vjg";
+        String mDetailURL;
+        String mPlaceId;
+        PlaceInfo mPlace;
+
+        LoadDetailInfo(String _detailURL, String _placeId, PlaceInfo _place)
+        {
+            mDetailURL = _detailURL;
+            mPlaceId = _placeId;
+            mPlace = _place;
+        }
+
+        String getContent(String path)
+        {
+            BufferedReader reader = null;
+            try
+            {
+                URL url = new URL(path);
+                HttpURLConnection c=(HttpURLConnection)url.openConnection();
+                c.setRequestMethod("GET");
+                c.setReadTimeout(10000);
+                c.connect();
+                reader= new BufferedReader(new InputStreamReader(c.getInputStream()));
+                StringBuilder buf=new StringBuilder();
+                String line=null;
+                while ((line=reader.readLine()) != null) {
+                    //Log.d("PlacesSearch",line);
+                    buf.append(line + "\n");
+                }
+                return(buf.toString());
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                if(reader != null)
+                {
+                    try {
+                        reader.close();
+                    }
+                    catch (Exception e)
+                    {e.printStackTrace();}
+                }
+            }
+            return null;
+        }
+        String prepareURL()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.append(mDetailURL);
+            result.append("placeid="+mPlaceId+"&");
+            result.append("language=ru&");
+            result.append("key="+KEY);
+
+            return result.toString();
+        }
+
+        @Override
+        protected String doInBackground(String... path)
+        {
+            String content;
+            try
+            {
+                content = getContent(prepareURL());
+                return content;
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result)
+        {
+            JSONObject jsonObject = null;
+
+            try
+            {
+                jsonObject = new JSONObject(result);
+                JSONObject DetailResult = jsonObject.getJSONObject("result");
+                mPlace.setPhoneNumber(DetailResult.getString("formatted_phone_number"));
+                mPlace.setPlaceAddress(DetailResult.getString("formatted_address"));
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
 }
